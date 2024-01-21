9ed742b7ccecb5c2a57b4489fc44abd2e8c735f8
mackay <johnmackay150@gmail.com>
2024-01-22 00:37:09 +0900

    CHANGE:OAuth options

--- src/lib/google/auth-client.ts
+++ src/lib/google/auth-client.ts
@@ -6,13 +6,20 @@ const CLIENT_SECRET = process.env.GOOGLE_API_CLIENT_SECRET || "";
 export const REDIRECT_URI =
   "https://google-oauth-example.vercel.app/api/auth/google-oauth/callback";
 
+const OPTIONS = {
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  redirectUri: REDIRECT_URI,
+};
+
 export function createOAuth2Client(options?: {
   clientId?: string;
   clientSecret?: string;
   redirectUri?: string;
 }) {
-  const clientId = options?.clientId || CLIENT_ID;
-  const clientSecret = options?.clientSecret || CLIENT_SECRET;
-  const redirectUri = options?.redirectUri || REDIRECT_URI;
+  const { clientId, clientSecret, redirectUri } = {
+    ...OPTIONS,
+    ...options,
+  };
   return new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 }
