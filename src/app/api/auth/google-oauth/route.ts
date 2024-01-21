ce69bb97bdfd243be2bf11041e4792909c6ecefa
mackay <johnmackay150@gmail.com>
2024-01-22 00:25:55 +0900

    ADD:optional oauth client

--- src/app/api/auth/google-oauth/route.ts
+++ src/app/api/auth/google-oauth/route.ts
@@ -8,21 +8,17 @@ const scopes = [
   "https://www.googleapis.com/auth/spreadsheets.readonly",
 ];
 
-const redirectUri =
-  "https://google-oauth-example.vercel.app/api/auth/google-oauth/callback";
-
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const originalUrl = searchParams.get("originalUrl") || "/";
 
   cookies().set("oauth2-redirect-original-url", originalUrl);
 
-  const oauth2Client = createOAuth2Client({ redirectUri });
+  const oauth2Client = createOAuth2Client();
 
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
     scope: scopes,
-    redirect_uri: redirectUri,
   });
 
   redirect(url);
--- src/lib/google/auth-client.ts
+++ src/lib/google/auth-client.ts
@@ -3,13 +3,16 @@ import { google } from "googleapis";
 const CLIENT_ID = process.env.GOOGLE_API_CLIENT_ID || "";
 const CLIENT_SECRET = process.env.GOOGLE_API_CLIENT_SECRET || "";
 
-export function createOAuth2Client(options: {
+export const REDIRECT_URI =
+  "https://google-oauth-example.vercel.app/api/auth/google-oauth/callback";
+
+export function createOAuth2Client(options?: {
   clientId?: string;
   clientSecret?: string;
   redirectUri?: string;
 }) {
-  const clientId = options.clientId || CLIENT_ID;
-  const clientSecret = options.clientSecret || CLIENT_SECRET;
-  const redirectUri = options.redirectUri;
+  const clientId = options?.clientId || CLIENT_ID;
+  const clientSecret = options?.clientSecret || CLIENT_SECRET;
+  const redirectUri = options?.redirectUri || REDIRECT_URI;
   return new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 }
